0407fadc2fb48f6f03a4bde073b8bfadf86ae62d
NOYA12345 <noyatavori15@gmail.com>
2024-02-01 13:26:49 +0200

    Initial commit

---
+++ connectDB.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+  cnx = mysql.connector.connect(user='root',password='root',
+                                database='liordb')
+  print(cnx.get_server_info())
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+
+
+
+if cnx and cnx.is_connected():
+
+    with cnx.cursor() as cursor:
+
+        result = cursor.execute("SELECT * FROM users WHERE user_id = '%s' AND password = '%s'")
+
+        rows = cursor.fetchall()
+
+        for rows in rows:
+
+            print(rows)
+
+    cnx.close()
+
+else:
+
+    print("Could not connect")
