7b000ea58d521062ff42be5677adac379d22ffd6
liorthekiler <liorkroytero@gmail.com>
2024-06-24 13:49:29 +0300

    delete commente

--- main.py
+++ main.py
@@ -758,20 +758,16 @@ while run:
                 white_pieces = pieces_list.copy()
                 black_pieces = pieces_list.copy()
 
+                opponent_pieces_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                                             (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+
+                player_pieces_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                                           (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
                 if connection.player_role == "0":
                     white_locations, black_locations = player_pieces_locations, opponent_pieces_locations
                 else:
                     black_locations, white_locations = player_pieces_locations, opponent_pieces_locations
-
-                # white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
-                #                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
-                # white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
-                #                    (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
-                # black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
-                #                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
-                # black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
-                #                    (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6),
-                #                    (6, 6), (7, 6)]
 
                 captured_pieces_white = []
                 captured_pieces_black = []
